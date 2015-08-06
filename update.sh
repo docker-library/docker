299e01eaf7185fd94f1c6b59c365867695caa828
Tianon Gravi <admwiggin@gmail.com>
2015-08-06 14:16:39 -0700

    More whoops

--- update.sh
+++ update.sh
@@ -37,6 +37,7 @@ for version in "${versions[@]}"; do
 			s/^(ENV DOCKER_VERSION) .*/\1 '"$fullVersion"'/;
 			s/^(ENV DOCKER_SHA256) .*/\1 '"$sha256"'/;
 			s/^(ENV DIND_COMMIT) .*/\1 '"$dindLatest"'/;
+			s/^(FROM docker):.*/\1:'"$version"'/;
 		' "$version/Dockerfile" "$version"/*/Dockerfile
 	)
 done
