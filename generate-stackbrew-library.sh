a98e0c42a96497670c36f4b2dcad2bcc81f18f35
Tianon Gravi <admwiggin@gmail.com>
2015-08-06 12:54:26 -0700

    Switch to alpine, add cool entrypoint scripts, support multiple versions

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 set -e
 
+declare -A aliases
+aliases=(
+	[1.7]='1 latest'
+	[1.8-rc]='rc'
+)
+
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+versions=( */ )
+versions=( "${versions[@]%/}" )
 url='git://github.com/docker-library/docker'
 
+echo '# maintainer: Tianon Gravi <tianon@dockerproject.org> (@tianon)'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
-commit="$(git log -1 --format='format:%H' -- .)"
-fullVersion="$(grep -m1 'ENV DOCKER_VERSION ' Dockerfile | cut -d' ' -f3)"
-
-versionAliases=()
-while [ "${fullVersion%[.-]*}" != "$fullVersion" ]; do
-	versionAliases+=( $fullVersion )
-	fullVersion="${fullVersion%[.-]*}"
-done
-versionAliases+=( $fullVersion latest )
-
-echo
-for va in "${versionAliases[@]}"; do
-	echo "$va: ${url}@${commit}"
-done
-
-for variant in dind git; do
-	commit="$(git log -1 --format='format:%H' -- "$variant")"
+for (( i = ${#versions[@]} - 1; i >= 0; --i )); do
+	version="${versions[$i]}"
+	
+	commit="$(git log -1 --format='format:%H' -- "$version")"
+	fullVersion="$(grep -m1 'ENV DOCKER_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
+	versionAliases=( $fullVersion $version ${aliases[$version]} )
+	
 	echo
 	for va in "${versionAliases[@]}"; do
-		if [ "$va" = 'latest' ]; then
-			va="$variant"
-		else
-			va="$va-$variant"
-		fi
-		echo "$va: ${url}@${commit} $variant"
+		echo "$va: ${url}@${commit} $version"
+	done
+	
+	for variant in dind git; do
+		[ -f "$version/$variant/Dockerfile" ] || continue
+		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
+		echo
+		for va in "${versionAliases[@]}"; do
+			if [ "$va" = 'latest' ]; then
+				va="$variant"
+			else
+				va="$va-$variant"
+			fi
+			echo "$va: ${url}@${commit} $version/$variant"
+		done
 	done
 done
-
-echo
-echo '# "supported": one tag per major, only upstream-supported majors (which is currently only "latest")'
--- update.sh
+++ update.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
-bucket="${1:-get.docker.com}"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-current="$(curl -fsSL "https://${bucket}/latest")"
-sha256="$(curl -fsSL "http://${bucket}/builds/Linux/x86_64/docker-${current}.sha256" | cut -d' ' -f1)"
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
 
 # "tac|tac" for http://stackoverflow.com/a/28879552/433558
-dindLatest="$(curl -sSL 'https://github.com/docker/docker/commits/master/hack/dind.atom'| tac|tac | awk -F '[[:space:]]*[<>/]+' '$2 == "id" && $3 ~ /Commit/ { print $4; exit }')"
+dindLatest="$(curl -fsSL 'https://github.com/docker/docker/commits/master/hack/dind.atom'| tac|tac | awk -F '[[:space:]]*[<>/]+' '$2 == "id" && $3 ~ /Commit/ { print $4; exit }')"
 
-(
-	set -x
-
-	sed -ri '
-		s/^(ENV DOCKER_BUCKET) .*/\1 '"$bucket"'/;
-		s/^(ENV DOCKER_VERSION) .*/\1 '"$current"'/;
-		s/^(ENV DOCKER_SHA256) .*/\1 '"$sha256"'/;
-
-		s/^(FROM docker):.*/\1:'"$current"'/;
-		s/^(ENV DIND_COMMIT) .*/\1 '"$dindLatest"'/;
-	' {,dind/,git/}Dockerfile
-)
+for version in "${versions[@]}"; do
+	rcGrepV='-v'
+	rcVersion="${version%-rc}"
+	if [ "$rcVersion" != "$version" ]; then
+		rcGrepV=
+	fi
+	fullVersion="$(git ls-remote --tags https://github.com/docker/docker.git | cut -d$'\t' -f2 | grep '^refs/tags/v[0-9].*\^{}$' | sed 's!^refs/tags/v!!; s!\^{}$!!' | grep $rcGrepV -- '-rc' | grep "^$rcVersion[.]" | sort -rV | head -n1)"
+	if [ -z "$fullVersion" ]; then
+		echo >&2 "warning: cannot find full version for $version"
+		continue
+	fi
+	bucket='get.docker.com'
+	if [ "$rcVersion" != "$version" ]; then
+		bucket='test.docker.com'
+	fi
+	sha256="$(curl -fsSL "https://$bucket/builds/Linux/x86_64/docker-$fullVersion.sha256" | cut -d' ' -f1)"
+	(
+		set -x
+		sed -ri '
+			s/^(ENV DOCKER_BUCKET) .*/\1 '"$bucket"'/;
+			s/^(ENV DOCKER_VERSION) .*/\1 '"$fullVersion"'/;
+			s/^(ENV DOCKER_SHA256) .*/\1 '"$sha256"'/;
+			s/^(ENV DIND_COMMIT) .*/\1 '"$dindLatest"'/;
+		' "$version/Dockerfile" "$version"/*/Dockerfile
+	)
+done
