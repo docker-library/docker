0437f58a50d2558d61751461c8d47aeef318f0bd
Tianon Gravi <admwiggin@gmail.com>
2015-08-20 14:28:19 -0700

    Update 1.8 entrypoint to use "docker daemon" instead of "--daemon"

--- 1.8/dind/dockerd-entrypoint.sh
+++ 1.8/dind/dockerd-entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 if [ "$#" -eq 0 -o "${1:0:1}" = '-' ]; then
-	set -- docker --daemon \
+	set -- docker daemon \
 		--host=unix:///var/run/docker.sock \
 		--host=tcp://0.0.0.0:2375 \
 		--storage-driver=vfs \
 		"$@"
 fi
 
-if [ "$1" = 'docker' ]; then
+if [ "$1" = 'docker' -a "$2" = 'daemon' ]; then
 	# if we're running Docker, let's pipe through dind
 	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
 	set -- sh "$(which dind)" "$@"
