5e30187978ad75d0f2ae5fc6c2a0b668bdf16885
Tianon Gravi <admwiggin@gmail.com>
2016-10-07 09:54:10 -0700

    Improve POSIX compliance of our entrypoint scripts

--- 1.12-rc/experimental/dind/dockerd-entrypoint.sh
+++ 1.12-rc/experimental/dind/dockerd-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
 
-if [ "$#" -eq 0 -o "${1:0:1}" = '-' ]; then
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
+	# add our default arguments
 	set -- docker daemon \
 		--host=unix:///var/run/docker.sock \
 		--host=tcp://0.0.0.0:2375 \
