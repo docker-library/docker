b81713203f68b0be14c55dcddf82af3367652526
Tianon Gravi <admwiggin@gmail.com>
2017-06-13 12:39:02 -0700

    Add a little "paperclip" suggestion when attempting to run "dockerd" in the non-dind image variants

--- 17.05/docker-entrypoint.sh
+++ 17.05/docker-entrypoint.sh
@@ -17,4 +17,19 @@ if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
 	export DOCKER_HOST='tcp://docker:2375'
 fi
 
+if [ "$1" = 'dockerd' ]; then
+	cat >&2 <<-'EOW'
+
+		📎 Hey there!  It looks like you're trying to run a Docker daemon.
+
+		   You probably should use the "dind" image variant instead, something like:
+
+		     docker run --privileged --name some-overlay-docker -d docker:stable-dind --storage-driver=overlay
+
+		   See https://hub.docker.com/_/docker/ for more documentation and usage examples.
+
+	EOW
+	sleep 3
+fi
+
 exec "$@"
