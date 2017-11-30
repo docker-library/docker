4dfb622e5f3b78f1dbb9de5a9a821146814c9032
Olaf Klischat <o.klischat@syseleven.de>
2017-12-01 00:20:16 +0100

    $DOCKERD_OPTIONS introduced

--- 17.11/dind/dockerd-entrypoint.sh
+++ 17.11/dind/dockerd-entrypoint.sh
@@ -8,13 +8,15 @@ if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
 	set -- dockerd \
 		--host=unix:///var/run/docker.sock \
 		--host=tcp://0.0.0.0:2375 \
+		$DOCKERD_OPTIONS \
 		"$@"
 fi
 
 if [ "$1" = 'dockerd' ]; then
 	# if we're running Docker, let's pipe through dind
 	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
-	set -- sh "$(which dind)" "$@"
+	shift
+	set -- sh "$(which dind)" dockerd $DOCKERD_OPTIONS "$@"
 fi
 
 exec "$@"
