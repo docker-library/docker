84fbcfc4cbef254dce2e7ecd7fcff788da4690df
Tianon Gravi <admwiggin@gmail.com>
2018-07-02 10:02:53 -0700

    Add 18.06.0-ce-rc1

---
+++ 18.06-rc/dind/dockerd-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
+	# add our default arguments
+	set -- dockerd \
+		--host=unix:///var/run/docker.sock \
+		--host=tcp://0.0.0.0:2375 \
+		"$@"
+fi
+
+if [ "$1" = 'dockerd' ]; then
+	# if we're running Docker, let's pipe through dind
+	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
+	set -- sh "$(which dind)" "$@"
+
+	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
+	rm -f /var/run/docker.pid
+fi
+
+exec "$@"
