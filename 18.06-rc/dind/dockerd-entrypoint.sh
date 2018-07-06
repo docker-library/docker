9ecb1c3a6bd766b69eb1858ef721f62fbd930a2b
Tianon Gravi <admwiggin@gmail.com>
2018-07-06 07:45:44 -0700

    Update pid deletion code to also delete "docker-containerd.pid"

--- 18.06-rc/dind/dockerd-entrypoint.sh
+++ 18.06-rc/dind/dockerd-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # no arguments passed
 # or first arg is `-f` or `--some-option`
-if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 	# add our default arguments
 	set -- dockerd \
 		--host=unix:///var/run/docker.sock \
@@ -17,7 +17,7 @@ if [ "$1" = 'dockerd' ]; then
 	set -- sh "$(which dind)" "$@"
 
 	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
-	rm -f /var/run/docker.pid
+	find /run /var/run -iname 'docker*.pid' -delete
 fi
 
 exec "$@"
