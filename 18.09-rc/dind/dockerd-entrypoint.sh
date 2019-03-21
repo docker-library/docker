6e5ccf3c71e86b98bb75600b4309ccf1bc87775e
Tianon Gravi <admwiggin@gmail.com>
2019-03-21 15:02:58 -0700

    Add 18.09.4-rc1

---
+++ 18.09-rc/dind/dockerd-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+	# add our default arguments
+	set -- dockerd \
+		--host=unix:///var/run/docker.sock \
+		--host=tcp://0.0.0.0:2375 \
+		"$@"
+fi
+
+if [ "$1" = 'dockerd' ]; then
+	# if we're running Docker, let's pipe through dind
+	set -- "$(which dind)" "$@"
+
+	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
+	find /run /var/run -iname 'docker*.pid' -delete
+fi
+
+exec "$@"
