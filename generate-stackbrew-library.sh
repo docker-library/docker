34503d72ae68c42a5cf3bcef16e2557379970f31
Tianon Gravi <admwiggin@gmail.com>
2019-04-11 16:34:12 -0700

    Adjust "generate-stackbrew-library.sh" to auto-remove RC releases when they go GA

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -64,6 +64,15 @@ for version in "${versions[@]}"; do
 
 	fullVersion="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "ENV" && $2 == "DOCKER_VERSION" { print $3; exit }')"
 
+	if [ "$rcVersion" != "$version" ] && [ -e "$rcVersion/Dockerfile" ]; then
+		# if this is a "-rc" release, let's make sure the release it contains isn't already GA (and thus something we should not publish anymore)
+		rcFullVersion="$(git show HEAD:"$rcVersion/Dockerfile" | awk '$1 == "ENV" && $2 == "DOCKER_VERSION" { print $3; exit }')"
+		if [[ "$fullVersion" == "$rcFullVersion"* ]]; then
+			# "x.y.z-rc1" == x.y.z*
+			continue
+		fi
+	fi
+
 	versionAliases=()
 	if [ "$version" = "$rcVersion" ]; then
 		while [ "$fullVersion" != "$rcVersion" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
