7d4fc89a7015b15f13dc09b2eec1670e01951081
Joshua Hoblitt <josh@hoblitt.com>
2019-04-15 15:57:12 -0700

    add DOCKER_HOST as a build-arg
    The current default is for dockerd to be world accessible.  This is
    sub-optimal for many use scenarios and can not be overridden without
    editing the Dockerfile. Per `docker info`:
    
        WARNING: API is accessible on http://0.0.0.0:2375 without encryption.
                 Access to the remote API is equivalent to root access on the host. Refer
                 to the 'Docker daemon attack surface' section in the documentation for
                 more information: https://docs.docker.com/engine/security/security/#docker-daemon-attack-surface
    
    This changset preserves the existing default build behavior while
    enabling the construction of more restricted image. Eg.,
    
        docker build . --build-arg DOCKER_HOST=tcp://127.0.0.1:2375

--- 18.06/dind/dockerd-entrypoint.sh
+++ 18.06/dind/dockerd-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
 
+DOCKER_HOST=${DOCKER_HOST:-tcp://0.0.0.0:2375}
+
 # no arguments passed
 # or first arg is `-f` or `--some-option`
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 	# add our default arguments
 	set -- dockerd \
 		--host=unix:///var/run/docker.sock \
-		--host=tcp://0.0.0.0:2375 \
+		--host="${DOCKER_HOST}" \
 		"$@"
 fi
 
