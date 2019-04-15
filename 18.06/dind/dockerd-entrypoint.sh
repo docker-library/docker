5ccd43cdc1258991644b3c982de8b13b26647a31
Joshua Hoblitt <josh@hoblitt.com>
2019-04-15 15:35:33 -0700

    use sh builtin `command` instead of `which`

--- 18.06/dind/dockerd-entrypoint.sh
+++ 18.06/dind/dockerd-entrypoint.sh
@@ -13,7 +13,7 @@ fi
 
 if [ "$1" = 'dockerd' ]; then
 	# if we're running Docker, let's pipe through dind
-	set -- "$(which dind)" "$@"
+	set -- "$(command -v dind)" "$@"
 
 	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
 	find /run /var/run -iname 'docker*.pid' -delete
